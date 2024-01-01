4b4b5ac86b4d7fff46edca465de9b3252680f4ac
RickChen13 <rickchen13@gmail.com>
2024-01-01 20:15:46 +0800

    将后台API类简化后再加载web程序

--- WinWallpaper/NodeJs/wallpaper/vue-wallpaper/src/main.ts
+++ WinWallpaper/NodeJs/wallpaper/vue-wallpaper/src/main.ts
@@ -1,18 +1,14 @@
 import { createApp } from "vue";
 import App from "@/app/App.vue";
 import router from "@/app/router";
-// import store from "@/app/store";
-// import ElementPlus from "element-plus";
-// import zhCn from "element-plus/es/locale/lang/zh-cn";
-// import "element-plus/dist/index.css";
 import "bootstrap-icons/font/bootstrap-icons.scss";
 
-const app = createApp(App);
-
-// app.use(store);
-app.use(router);
-// app.use(ElementPlus, {
-//   locale: zhCn,
-// });
-
-app.mount("#app");
+const setApi = async () => {
+    window.DesktopJsApi = window.chrome.webview.hostObjects.DesktopJsApi
+}
+(async () => {
+    await setApi();
+    const app = createApp(App);
+    app.use(router);
+    app.mount("#app");
+})()
